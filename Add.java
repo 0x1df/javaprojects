c3a7aef33614cbe2e1808d0717d95d1047429c23
0x1df <0x1df@proton.me>
2023-08-20 14:01:52 +0530

    h

---
+++ Add.java
@@ -0,0 +1,67 @@
+public class Add {
+    public static void main(String[]args){
+        int a=2,b=3,c=3;
+        System.out.println(a*b/c);
+    }
+}
+
+
+/*the arthemetic operations in java follow as 
+    Postfix Operators:
+        expression++
+        expression--
+
+    Unary Operators:
+        ++expression
+        --expression
+        +expression (unary plus)
+        -expression (unary minus)
+        !expression (logical NOT)
+        ~expression (bitwise NOT)
+        (type) expression (casting)
+
+    Multiplicative Operators:
+        * (multiplication)
+        / (division)
+        % (modulo)
+
+    Additive Operators:
+        + (addition)
+        - (subtraction)
+
+    Shift Operators:
+        << (left shift)
+        >> (right shift)
+        >>> (unsigned right shift)
+
+    Relational Operators:
+        < (less than)
+        > (greater than)
+        <= (less than or equal to)
+        >= (greater than or equal to)
+        instanceof (type comparison)
+
+    Equality Operators:
+        == (equal to)
+        != (not equal to)
+
+    Bitwise AND Operator:
+        &
+
+    Bitwise XOR Operator:
+        ^
+
+    Bitwise OR Operator:
+        |
+
+    Logical AND Operator:
+        &&
+
+    Logical OR Operator:
+        ||
+
+    Conditional Operator:
+        ? : (ternary conditional)
+
+    Assignment Operators:
+        =, +=, -=, *=, /=, %=, &=, ^=, |=, <<=, >>=, >>>=*/

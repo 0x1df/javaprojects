19a5389228bfe29730aacbc7f4b7663e5f0132f4
0x1df <0x1df@proton.me>
2023-08-25 03:58:52 +0530

    simple Inputtaker

---
+++ Inputtaker.java
@@ -0,0 +1,87 @@
+/*import java.util.Scanner;
+public class Inputtaker{
+    public static void main(String[]args){
+
+Scanner enter = new Scanner(System.in);
+
+System.out.println("enter the the number you want to store ");
+int a = enter.nextInt();
+System.out.println("enter another value");
+int b =enter.nextInt();
+int sum = a+b;
+System.out.println("the answer is ");
+System.out.println(sum);
+
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+import java.util.Scanner;
+public class Inputtaker{
+    public static void main(String[]args){
+    Scanner enter = new Scanner(System.in);
+    System.out.println("enter the number youru wsnna store ");
+    int b=enter.nextInt();
+
+    System.out.println("enter the another number ");
+    int i=enter.nextInt();
+    System.out.println("enter the another number ");
+    int j=enter.nextInt();
+    System.out.println("enter the another number ");
+    int k=enter.nextInt();
+    System.out.println("enter the another number ");
+    int l=enter.nextInt();
+
+
+
+
+
+int avg= b+i+j+k+l;
+int avera= avg/2;
+System.out.println("the avg of yoyre marks are ");
+System.out.println(avera);
+
+
+
+
+
+
+
+
+
+
+    }
+}
+*/
+
+
+
+import java.util.Scanner;
+public class Inputtaker{
+public static void main(String[]args){
+    Scanner enter= new Scanner(System.in);
+    System.out.println("enter the value");
+boolean babu= enter.hasNextInt();
+    System.out.println(babu);
+
+}
+}
